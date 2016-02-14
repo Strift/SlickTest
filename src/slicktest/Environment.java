74d2cda0ef0ecefc1f01ce992ebe99f813f0f794
elythiel <mickael.o.maillot@gmail.com>
2016-02-14 23:47:25 +0400

    added a camera centered on the player (not finished)

--- src/slicktest/Environment.java
+++ src/slicktest/Environment.java
@@ -10,7 +10,7 @@ import org.newdawn.slick.SlickException;
 
 public class Environment extends BasicGame {
 	
-	ArrayList<Map> maps ;
+	private ArrayList<Map> maps ;
 	
 	private Player player ;
 	
@@ -19,15 +19,15 @@ public class Environment extends BasicGame {
 	}
 	
 	@Override
-	public void update(GameContainer arg0, int arg1) throws SlickException {
+	public void update(GameContainer gc, int arg1) throws SlickException {
 		if(player.isMoving()) {
         	if(player.getDirection() == 0) {
         	} else if (player.getDirection() == 1) {
-        		if (player.getLocationX() <= 800-32) 
+        		if (player.getLocationX() <= maps.get(0).getWidth()*maps.get(0).getTileWidth()-32) 
         			this.player.setLocationX(player.getLocationX()+player.getMoveSpeed());
         	} else if (player.getDirection() == 2) {
         	} else if (player.getDirection() == 3) {
-        		if (player.getLocationX() >= 0)
+        		if (player.getLocationX() > 1)
         			this.player.setLocationX(player.getLocationX()-player.getMoveSpeed());
         	}
     	}
@@ -35,22 +35,36 @@ public class Environment extends BasicGame {
 
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException {
-		this.maps.get(0).render(0, 0); ;
+		this.centerCamera(gc, g);
+		this.maps.get(0).render(0, 0);
     	g.drawAnimation(
     		this.player.getAnimations()[player.getDirection() + (this.player.isMoving() ? 4 : 0)], 
 			this.player.getLocationX(), 
 			this.player.getLocationY()
 		);
 	}
+	
+	/**
+	 * This method moves all the Graphics using the player location.
+	 * Player is always at center of the screen (x axe).
+	 * @param gc Game Container
+	 * @param g Graphics
+	 */
+	private void centerCamera(GameContainer gc, Graphics g) {
+		int cameraLocation = gc.getWidth() / 2 - this.player.getLocationX() ;
+		if(cameraLocation < 0) {
+			g.translate(cameraLocation, 0);
+		}
+	}
 
 	@Override
-	public void init(GameContainer arg0) throws SlickException {
+	public void init(GameContainer gc) throws SlickException {
 		// TODO Auto-generated method stub
 		this.maps = new ArrayList<Map>();
 		this.maps.add(new Map("/maps/map2d.tmx")) ;
 		
 		this.player = new Player("Martelle", "/images/sonic.png", 24, 32) ;
-    	this.player.setLocation(0, 96);
+    	this.player.setLocation(1, gc.getHeight()-8*16);
     	    	
 	}
 	
