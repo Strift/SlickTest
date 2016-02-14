c1cb9d4d2744db8d7014e9fa1c76ba82c92e1cc6
Laurent <lau.cazanove@gmail.com>
2016-02-14 21:30:16 +0100

    Separated Environment from Game class

--- src/slicktest/Environment.java
+++ src/slicktest/Environment.java
@@ -2,24 +2,21 @@ package slicktest;
 
 import java.util.ArrayList;
 
-import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
-public class Environment extends BasicGame {
+public class Environment {
 	
 	private ArrayList<Map> maps ;
-	
 	private Player player ;
 	
-	public Environment(String title) {
-		super(title);
+	public Environment() throws SlickException {
+		this.maps = new ArrayList<Map>();
+		this.maps.add(new Map("/maps/map2d.tmx")) ;
+		this.player = new Player("Martelle", "/images/sonic.png", 24, 32) ;
+    	this.player.setLocation(1, maps.get(0).getHeight()-8*16);
 	}
-	
-	@Override
-	public void update(GameContainer gc, int arg1) throws SlickException {
+
+	public void update() {
 		if(player.isMoving()) {
         	if(player.getDirection() == 0) {
         	} else if (player.getDirection() == 1) {
@@ -32,68 +29,20 @@ public class Environment extends BasicGame {
         	}
     	}
 	}
-
-	@Override
-	public void render(GameContainer gc, Graphics g) throws SlickException {
-		this.centerCamera(gc, g);
-		this.maps.get(0).render(0, 0);
-    	g.drawAnimation(
-    		this.player.getAnimations()[player.getDirection() + (this.player.isMoving() ? 4 : 0)], 
-			this.player.getLocationX(), 
-			this.player.getLocationY()
-		);
-	}
 	
-	/**
-	 * This method moves all the Graphics using the player location.
-	 * Player is always at center of the screen (x axe).
-	 * @param gc Game Container
-	 * @param g Graphics
-	 */
-	private void centerCamera(GameContainer gc, Graphics g) {
-		int cameraLocation = gc.getWidth() / 2 - this.player.getLocationX() ;
-		if(cameraLocation < 0) {
-			g.translate(cameraLocation, 0);
-		}
+	public Player getPlayer() {
+		return player;
 	}
 
-	@Override
-	public void init(GameContainer gc) throws SlickException {
-		// TODO Auto-generated method stub
-		this.maps = new ArrayList<Map>();
-		this.maps.add(new Map("/maps/map2d.tmx")) ;
-		
-		this.player = new Player("Martelle", "/images/sonic.png", 24, 32) ;
-    	this.player.setLocation(1, gc.getHeight()-8*16);
-    	    	
+	public void setPlayer(Player player) {
+		this.player = player;
 	}
-	
-    @Override
-    public void keyPressed(int key, char c) {
-    	if(key == Input.KEY_UP) {
-    		this.player.setDirection(0);
-    		this.player.setMoving(true);
-    	} else if (key == Input.KEY_DOWN) {
-    		this.player.setDirection(2) ;
-    		this.player.setMoving(true);
-    	} else if (key == Input.KEY_LEFT) {
-    		this.player.setDirection(3);
-    		this.player.setMoving(true);
-    	} else if (key == Input.KEY_RIGHT) {
-    		this.player.setDirection(1) ;
-    		this.player.setMoving(true);
-    	} else if (key == Input.KEY_SPACE) {
-    		this.player.setMoveSpeed(4);
-    	}
-    };
-    
-    @Override
-    public void keyReleased(int key, char c) {
-        if(key == Input.KEY_UP || key == Input.KEY_DOWN || key == Input.KEY_LEFT || key == Input.KEY_RIGHT) {
-        	this.player.setMoving(false);
-        } else if (key == Input.KEY_SPACE) {
-        	this.player.setMoveSpeed(2);
-        }
-    }
 
+	public ArrayList<Map> getMaps() {
+		return maps;
+	}
+
+	public void setMaps(ArrayList<Map> maps) {
+		this.maps = maps;
+	}
 }
---
+++ src/slicktest/Game.java
@@ -0,0 +1,81 @@
+package slicktest;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+
+public class Game extends BasicGame {
+	
+	private Environment environment;
+	
+	public Game(String title) {
+		super(title);
+	}
+	
+	@Override
+	public void update(GameContainer gc, int delta) throws SlickException {
+		environment.update();
+	}
+
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException {
+		this.centerCamera(gc, g);
+		environment.getMaps().get(0).render(0, 0);
+    	g.drawAnimation(
+    		environment.getPlayer().getAnimations()[environment.getPlayer().getDirection() + (environment.getPlayer().isMoving() ? 4 : 0)], 
+			environment.getPlayer().getLocationX(), 
+			environment.getPlayer().getLocationY()
+		);
+	}
+	
+	/**
+	 * This method moves all the Graphics using the player location.
+	 * Player is always at center of the screen (x axe).
+	 * @param gc Game Container
+	 * @param g Graphics
+	 */
+	private void centerCamera(GameContainer gc, Graphics g) {
+		int cameraLocation = gc.getWidth() / 2 - environment.getPlayer().getLocationX() ;
+		if(cameraLocation < 0) {
+			g.translate(cameraLocation, 0);
+		}
+	}
+
+	@Override
+	public void init(GameContainer gc) throws SlickException {
+		// TODO Auto-generated method stub
+		environment = new Environment();
+    	    	
+	}
+	
+    @Override
+    public void keyPressed(int key, char c) {
+    	if(key == Input.KEY_UP) {
+    		environment.getPlayer().setDirection(0);
+    		environment.getPlayer().setMoving(true);
+    	} else if (key == Input.KEY_DOWN) {
+    		environment.getPlayer().setDirection(2) ;
+    		environment.getPlayer().setMoving(true);
+    	} else if (key == Input.KEY_LEFT) {
+    		environment.getPlayer().setDirection(3);
+    		environment.getPlayer().setMoving(true);
+    	} else if (key == Input.KEY_RIGHT) {
+    		environment.getPlayer().setDirection(1) ;
+    		environment.getPlayer().setMoving(true);
+    	} else if (key == Input.KEY_SPACE) {
+    		environment.getPlayer().setMoveSpeed(4);
+    	}
+    };
+    
+    @Override
+    public void keyReleased(int key, char c) {
+        if(key == Input.KEY_UP || key == Input.KEY_DOWN || key == Input.KEY_LEFT || key == Input.KEY_RIGHT) {
+        	environment.getPlayer().setMoving(false);
+        } else if (key == Input.KEY_SPACE) {
+        	environment.getPlayer().setMoveSpeed(2);
+        }
+    }
+
+}
