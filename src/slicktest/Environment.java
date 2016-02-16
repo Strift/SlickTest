f039d1ff9e8f1a1fff016919511d9975c929cd97
Laurent <lau.cazanove@gmail.com>
2016-02-16 18:08:31 +0100

    Drawing fields for debug purposes

--- src/slicktest/Environment.java
+++ src/slicktest/Environment.java
@@ -2,6 +2,7 @@ package slicktest;
 
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Path;
 import org.newdawn.slick.tiled.TiledMap;
 
 import entities.PhysicalEntity;
@@ -33,6 +34,10 @@ public class Environment {
 				Player player = this.getPlayer();
 				g.drawAnimation(player.getAnimation(), player.getPosition().x, player.getPosition().y);
 			}
+		}
+		// Draw fields for debug purposes
+		for (Path path : map.getFields()) {
+			g.draw(path);
 		}
 	}
 	
